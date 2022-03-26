6a238e26fdbd9af2a8c199d4abbadbd1019c77c5
Daniel Lucas <daniel.lucasafj@gmail.com>
2022-03-26 12:36:42 -0300

    refactory method get_date_filter

--- src/main/java/br/com/banco/controllers/TransferenciaController.java
+++ src/main/java/br/com/banco/controllers/TransferenciaController.java
@@ -1,6 +1,5 @@
 package br.com.banco.controllers;
 
-
 import java.time.ZoneId;
 import java.time.ZonedDateTime;
 import java.util.ArrayList;
@@ -15,50 +14,45 @@ import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import br.com.banco.model.Transferencia;
+import br.com.banco.model.services.DateService;
+import br.com.banco.model.services.TransferenciaService;
 import br.com.banco.repository.TransferenciaRepository;
 
 @RestController
 @RequestMapping("/transferencia")
 public class TransferenciaController {
-		
+
 	@Autowired
 	private TransferenciaRepository transferenciaRepository;
-	
+
 	/**
-	 *  Método que faz a busca de todos os dados na Base de Dados
-	 *  
-	 * @return Todas as transferências 
+	 * Método que faz a busca de todos os dados na Base de Dados
+	 * 
+	 * @return Todas as transferências
 	 */
 	@GetMapping()
 	public List<Transferencia> listar_transferencia() {
-		
+
 		return transferenciaRepository.findAll();
 	}
-	
+
 	/**
 	 * Este método retorna uma lista de transferências com base nas datas informadas
+	 * 
 	 * @param data_ini
 	 * @param data_fim
 	 * @return Lista Transferência
 	 */
 	@GetMapping("/busca")
-	public List<Transferencia> get_data_filter(@RequestParam(name = "data_inicial") @DateTimeFormat(pattern = "yyyy-MM-dd") Date data_ini,
-			@RequestParam(name = "data_fim",required = false) @DateTimeFormat(pattern = "yyyy-MM-dd") Date data_fim) {
-		List<Transferencia> transferencias = transferenciaRepository.findAll();
-		List<Transferencia> busca = new ArrayList<Transferencia>();
-		ZonedDateTime data_inicial =data_ini.toInstant().atZone(ZoneId.systemDefault());
-	
+	public List<Transferencia> get_date_filter(
+			@RequestParam(name = "data_inicial") @DateTimeFormat(pattern = "yyyy-MM-dd") Date data_ini,
+			@RequestParam(name = "data_fim", required = false) @DateTimeFormat(pattern = "yyyy-MM-dd") Date data_fim) {
 
-		transferencias.forEach(transferencia ->{
-			Date data_banco = transferencia.getData_transferencia();
-			ZonedDateTime dbTime = data_banco.toInstant().atZone(ZoneId.systemDefault());
-					
-			if(dbTime.toLocalDate().equals(data_inicial.toLocalDate())) {
-				busca.add(transferencia);
-			}
-		});
-		return busca;
+		List<Transferencia> transferencias = transferenciaRepository.findAll();
+		TransferenciaService trans_service = new TransferenciaService();
+
+		trans_service.filtro_data(transferencias, data_ini);
+		return trans_service.filtro_data(transferencias, data_ini);
 	}
-	
-	
+
 }
