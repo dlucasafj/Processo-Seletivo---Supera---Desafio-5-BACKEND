aac12719a2a41dd12d3afb74f8a35d62c2b712c0
Daniel Lucas <daniel.lucasafj@gmail.com>
2022-03-26 13:35:25 -0300

    added endpoint for period search method

--- src/main/java/br/com/banco/controllers/TransferenciaController.java
+++ src/main/java/br/com/banco/controllers/TransferenciaController.java
@@ -1,8 +1,5 @@
 package br.com.banco.controllers;
 
-import java.time.ZoneId;
-import java.time.ZonedDateTime;
-import java.util.ArrayList;
 import java.util.Date;
 import java.util.List;
 
@@ -14,7 +11,6 @@ import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import br.com.banco.model.Transferencia;
-import br.com.banco.model.services.DateService;
 import br.com.banco.model.services.TransferenciaService;
 import br.com.banco.repository.TransferenciaRepository;
 
@@ -51,8 +47,28 @@ public class TransferenciaController {
 		List<Transferencia> transferencias = transferenciaRepository.findAll();
 		TransferenciaService trans_service = new TransferenciaService();
 
-		trans_service.filtro_data(transferencias, data_ini);
+//		trans_service.filtro_data(transferencias, data_ini);
 		return trans_service.filtro_data(transferencias, data_ini);
+	}
+
+	/**
+	 * Método responsável por fazer uma busca dos dados de acordo com o período
+	 * informado
+	 * 
+	 * @param data_ini -> Data inicial
+	 * @param data_fim -> Data final
+	 * @return Lista de Transferência referente ao período
+	 */
+	@GetMapping("/busca_periodo")
+	public List<Transferencia> get_data_por_periodo(
+			@RequestParam(name = "data_inicial") @DateTimeFormat(pattern = "yyyy-MM-dd") Date data_ini,
+			@RequestParam(name = "data_fim") @DateTimeFormat(pattern = "yyyy-MM-dd") Date data_fim) {
+
+		List<Transferencia> transferencias = transferenciaRepository.findAll();
+		TransferenciaService trans_service = new TransferenciaService();
+
+		return trans_service.filtro_periodo(transferencias, data_ini, data_fim);
+
 	}
 
 }
